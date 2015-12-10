7a42a1fdc82792173ee36a0324c2e7bcfd2a1dc1
kaessajidi <kamalessajidi@htomail.com>
2015-12-10 21:39:36 +0100

    merge

--- src/net/sf/jadclipse/PTCDecompiler.java
+++ src/net/sf/jadclipse/PTCDecompiler.java
@@ -9,16 +9,23 @@ import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import net.sf.jadclipse.opengrock.Utils;
+
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  * @author kamal
- *
+ * 
  */
 public class PTCDecompiler implements IDecompiler {
-	
+
 	private String source = "";
 	private StringBuffer log;
 	private List excList = new ArrayList();
@@ -31,169 +38,196 @@ public class PTCDecompiler implements IDecompiler {
 		// TODO Auto-generated constructor stub
 	}
 
-	/* (non-Javadoc)
-	 * @see net.sf.jadclipse.IDecompiler#decompile(java.lang.String, java.lang.String, java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.sf.jadclipse.IDecompiler#decompile(java.lang.String,
+	 * java.lang.String, java.lang.String)
 	 */
 	public void decompile(String root, String packege, String className) {
-		
-		IPreferenceStore prefs = JadclipsePlugin.getDefault().getPreferenceStore();    	
+
+		IPreferenceStore prefs = JadclipsePlugin.getDefault().getPreferenceStore();
 		String srcUrl = prefs.getString(JadclipsePlugin.PTC_URL);
-		String srcVersion = prefs.getString(JadclipsePlugin.PTC_VERSION);
-		String repositoryType = null;
+		String gproject = prefs.getString(JadclipsePlugin.GROCK_PROJECT);
+
 		try {
-			start = System.currentTimeMillis();   
-	        // Construct data
-	        String data = URLEncoder.encode(packege + "." + className, "UTF-8");
-	        String path = data.replaceAll("\\.", "/") + ".java";
-	        data += "&" + URLEncoder.encode("ln", "UTF-8") + "=" + URLEncoder.encode("false", "UTF-8");
-	        
-	        if(srcUrl.indexOf("ah-grok") != -1)
-	        	repositoryType = "opengroc";
-	        else
-	        	repositoryType = "whereUserd";
-	        		
-	        // Send data
-	        srcUrl = srcUrl.replaceAll("@class@", data).replaceAll("@path@", path);
-	        	
-	        URL url = new URL( srcUrl );
-	        URLConnection conn = url.openConnection();
-	        conn.setDoOutput(true);
-	       
-	        if(repositoryType.equals("whereUserd"))
-        	{
-		        OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-		        wr.write(data);
-		        wr.flush();
-		    
-		        // Get the response
-		        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		        String line;
-		        boolean go = false;
-		        
-		        while ((line = rd.readLine()) != null) {
-		        	
-		        		if(go)
-		        			source += replaceAll(line) + "\n";       		
-		        		
-		        		if( line.startsWith("<pre>") )
-		        			go = true;
-		        		if( line.startsWith("</pre>") )
-		        			go = false;	        	
-		        		  
-		        }
-		        
-		        wr.close();
-		        rd.close();	        
-        	} else
-        	{
-		        String line;
-		        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		        while ((line = rd.readLine()) != null) {
-        			source += line + "\n";       		
-		        }
-		        rd.close();
-        	}
-	        
-	        
-	    } catch (Exception e) {
-	    	
-	    	e.printStackTrace();
-	    	
-	    }finally
-    	{
-			time = System.currentTimeMillis() - start;    		
-    	}
+			start = System.currentTimeMillis();
+
+			// Construct data
+			String data = URLEncoder.encode(packege + "." + className, "UTF-8");
+			String path = data.replaceAll("\\.", "/") + ".java";
+		
+			if (Utils.isWhereUsedSite(srcUrl)) {
+				// Send data
+				//http://ah-wused.ptcnet.ptc.com/wus_x-24_M032/viewSource.jsp?class=@class@
+				
+				srcUrl = srcUrl + "/" + gproject + "/viewSource.jsp?class=" + packege + "." + className;
+				data += "&" + URLEncoder.encode("ln", "UTF-8") + "=" + URLEncoder.encode("false", "UTF-8");
+				
+				System.out.println("Open: " + srcUrl);
+				
+				URL url = new URL(srcUrl);
+				URLConnection conn = url.openConnection();
+				conn.setDoOutput(true);
+
+				OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+				wr.write(data);
+				wr.flush();
+
+				// Get the response
+				BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				String line;
+				boolean go = false;
+
+				while ((line = rd.readLine()) != null) {
+
+					if (go)
+						source += replaceAll(line) + "\n";
+
+					if (line.startsWith("<pre>"))
+						go = true;
+					if (line.startsWith("</pre>"))
+						go = false;
+
+				}
+
+				wr.close();
+				rd.close();
+			} else {
+				String line;
+				String search_pattern = srcUrl + "search?q=&project=" + gproject + "&defs=&refs=&path=" + path + "&hist=";
+
+				/*
+				 * get the file path source
+				 */
+
+				Document doc = Jsoup.connect(search_pattern).get();
+				Elements results = doc.getElementById("results").getAllElements();
+				for (Element el : results) {
+					if (el.tagName().equals("a") && el.parent().className().equals("f")) {
+						String class_url = srcUrl + el.attr("href").replaceAll("xref", "opengrok_src");
+						
+						System.out.println("Open: " + class_url);
+
+						URL url = new URL(class_url);
+						URLConnection conn = url.openConnection();
+						conn.setDoOutput(true);
+						BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+						try {
+							while ((line = rd.readLine()) != null) {
+								source += line + "\n";
+							}
+						} finally {
+							rd.close();
+						}
+						/*
+						 * Stop at first
+						 */
+						break;
+
+					}
+				}
+
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		} finally {
+			time = System.currentTimeMillis() - start;
+		}
 
 	}
 
-	public String replaceAll ( String line){
-		String[] replaces  = { 
-		 ">",
-		 "<",
-		 "&",
-		 ""
-		};
-		
-		String[] paterns = {
-			"&gt;",
-			"&lt;",
-			"&amp;",			
-			"<[\\/\\!]*?[^<>]*?>"			
-		};
-		
+	public String replaceAll(String line) {
+		String[] replaces = { ">", "<", "&", "" };
+
+		String[] paterns = { "&gt;", "&lt;", "&amp;", "<[\\/\\!]*?[^<>]*?>" };
+
 		for (int i = 0; i < paterns.length; i++) {
 			line = line.replaceAll(paterns[i], replaces[i]);
 		}
-		
+
 		return line;
 	}
-	/* (non-Javadoc)
-	 * @see net.sf.jadclipse.IDecompiler#decompileFromArchive(java.lang.String, java.lang.String, java.lang.String)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.sf.jadclipse.IDecompiler#decompileFromArchive(java.lang.String,
+	 * java.lang.String, java.lang.String)
 	 */
-	public void decompileFromArchive(String archivePath, String packege,
-			String className) {
+	public void decompileFromArchive(String archivePath, String packege, String className) {
 		// TODO Auto-generated method stub
 
 	}
 
-	 /**
-     * @see IDecompiler#getSource()
-     */
-    public String getSource()
-    {
-        return source;
-    }
+	/**
+	 * @see IDecompiler#getSource()
+	 */
+	public String getSource() {
+		return source;
+	}
 
-    /**
-     * @see IDecompiler#getLog()
-     */
-    public String getLog()
-    {
-        return log == null ? "" : log.toString();
-    }
-    
-    public List getExceptions()
-    {
-    	return excList;
-    }
-    
-    public long getDecompilationTime()
-    {
-    	return time;
-    }
-    
+	/**
+	 * @see IDecompiler#getLog()
+	 */
+	public String getLog() {
+		return log == null ? "" : log.toString();
+	}
+
+	public List getExceptions() {
+		return excList;
+	}
+
+	public long getDecompilationTime() {
+		return time;
+	}
+
 	/**
 	 * @param args
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
-		
-			String packege = "wt.part";
-			String className = "WTPart";
-			String srcVersion = "";
-			
-		 	String data = URLEncoder.encode("class", "UTF-8") + "=" + URLEncoder.encode(packege + "." + className, "UTF-8");
-	        data += "&" + URLEncoder.encode("version", "UTF-8") + "=" + URLEncoder.encode(srcVersion, "UTF-8");
-	        String msource = "";
-	        // Send data
-	        URL url = new URL( "http://ah-wused.ptcnet.ptc.com/wus_x-05_M030/wuscs.jsp" + "?" + data );
-	        URLConnection conn = url.openConnection();
-	        conn.setDoOutput(true);
-	        OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-	        wr.write(data);
-	        wr.flush();
-	    
-	        // Get the response
-	        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-	        String line;
-	        while ((line = rd.readLine()) != null) {
-	        	//no html
-	        	line = line.replaceAll("<[\\/\\!]*?[^<>]*?>", "");
-	        	if( line.startsWith("  ") && line.indexOf('|') != -1 )
-	        		msource += line.substring(line.indexOf("| ") + 2) + "\n";
-	        }
-	        wr.close();
-	        rd.close();
+
+		String _package = ""; // "com.infoengine.connector";
+		String className = "Element";
+		String srcVersion = "";
+
+		String project = "x-26";
+		String file = _package.replace('.', '/') + "/" + className;
+		String grock_url = "http://ah-opengrok.ptcnet.ptc.com/";
+		String search_pattern = grock_url + "search?q=&project=" + project + "&defs=&refs=&path=" + file + ".java&hist=";
+
+		Document doc = Jsoup.connect(search_pattern).get();
+		Elements results = doc.getElementById("results").getAllElements();
+
+		// System.out.println(results.toString());
+
+		for (Element el : results) {
+			if (el.tagName().equals("a") && el.parent().className().equals("f")) {
+				System.out.println(el.attr("href"));
+
+				// http://ah-opengrok.ptcnet.ptc.com/xref/x-26/infoengine/InfoEngine/src/com/infoengine/object/factory/Element.java
+				// http://ah-opengrok.ptcnet.ptc.com/opengrok_src/x-26/infoengine/InfoEngine/src/com/infoengine/object/factory/Element.java
+
+				String class_url = grock_url + el.attr("href").replaceAll("xref", "opengrok_src");
+
+				URL url = new URL(class_url);
+				URLConnection conn = url.openConnection();
+				conn.setDoOutput(true);
+				String line;
+				BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				while ((line = rd.readLine()) != null) {
+					System.out.println(line);
+				}
+				rd.close();
+
+				// Document doc_class = Jsoup.connect(class_url).get();
+				// System.out.println(doc_class.text());
+			}
+		}
 
 	}
 
--- src/net/sf/jadclipse/opengrock/Utils.java
+++ src/net/sf/jadclipse/opengrock/Utils.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
+import net.sf.jadclipse.JadclipsePlugin;
+
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -16,6 +20,7 @@ public class Utils {
 		Map<String, String> list = new HashMap<String, String>();
 		list.put("http://ah-opengrok.ptcnet.ptc.com/", "http://ah-opengrok.ptcnet.ptc.com/");
 		list.put("http://bla-grok-01/", "http://bla-grok-01/");
+		list.put("http://ah-wused.ptcnet.ptc.com", "http://ah-wused.ptcnet.ptc.com");
 		list.put("http://localhost:8090/source/", "http://localhost:8090/source/");
 		return getArrayFromHash(list);
 	}
@@ -23,18 +28,24 @@ public class Utils {
 	public static String[][] getGrockProjects() {
 		// TODO Auto-generated method stub
 		
-		Map<String, String> list = new HashMap<String, String>();
+		Map<String, String> list = new TreeMap<String, String>();
 		try {
-			Document doc = Jsoup.connect("http://bla-grok-01/").get();
-			Elements projects = doc.getElementById("project").getAllElements();
-			
-			for (Element project : projects) {
-				if(project.tagName().equals("option"))
-					list.put(project.val(), project.text());
+			IPreferenceStore prefs = JadclipsePlugin.getDefault().getPreferenceStore(); 
+			String srcUrl = prefs.getString(JadclipsePlugin.PTC_URL);
+			Document doc = Jsoup.connect(prefs.getString(JadclipsePlugin.PTC_URL) + "/wus_x-20/wusSelectApp.jsp").get();
+			if (isWhereUsedSite(srcUrl)) {
+				Elements projects = doc.getElementsByTag("li");
+				for (Element project : projects) {
+					list.put(project.children().text(), project.children().attr("href").split("/")[1]);
+				}
+			} else {
+				Elements projects = doc.getElementById("project").getAllElements();
+				for (Element project : projects) {
+					if (project.tagName().equals("option"))
+						list.put(project.val(), project.text());
+				}
 			}
 
-			//el.baseUri();
-			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -43,7 +54,12 @@ public class Utils {
 		return getArrayFromHash(list);
 	}
 	
-	
+	public static boolean isWhereUsedSite(String srcUrl){
+		if (srcUrl.indexOf("ah-wused") == -1)
+			return false;
+		else
+			return true;
+	}
 	public static String[][] getArrayFromHash(Map<String,String> data){
         String[][] str = null;
         {
